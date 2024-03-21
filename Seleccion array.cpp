88729bd5e287c0bd6eeb3504e1dd2f752ea19a1b
UsiasSG <160407522+UsiasSG@users.noreply.github.com>
2024-03-20 22:39:07 -0600

    Add files via upload

---
+++ Seleccion array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main() {
+	
+	string semana [7]={"lunes", "Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+	    
+	     semana[3] = "jueves";
+	     for (int i=0; i<7; i++){
+	     	cout<<semana[i]<<endl;
+		 }
+		 
+		 system("pause");
+}
+
